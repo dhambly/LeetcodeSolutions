9648350c225bf7e26427303cad10919eefa012f9
dhambly <77180575+dhambly@users.noreply.github.com>
2021-01-29 23:11:18 -0600

    Update LRU Cache.java

--- LRU Cache.java
+++ LRU Cache.java
@@ -29,3 +29,32 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+
+class LRUCache extends LinkedHashMap<Integer,Integer> {
+    int cap = 0;
+    
+    public LRUCache(int capacity) {
+        super(capacity,0.75F,true);
+        cap = capacity;
+    }
+    
+    public int get(int key) {
+        return super.getOrDefault(key,-1);
+    }
+    
+    public void put(int key, int value) {
+        super.put(key,value);
+    }
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest) {
+        return size() > cap;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
