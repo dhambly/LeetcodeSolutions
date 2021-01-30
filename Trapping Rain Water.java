634a913baca79cba5f219a36c9d06e267f3204c0
dhambly <77180575+dhambly@users.noreply.github.com>
2021-01-29 23:22:08 -0600

    Update Trapping Rain Water.java

--- Trapping Rain Water.java
+++ Trapping Rain Water.java
@@ -1,30 +1,20 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        
         if (n == 0) return 0;
         
-        int[] leftheight = new int[n];
-        int[] rightheight = new int[n];
-        
-        
-        
-        leftheight[0] = height[0];
-        
-        for (int i = 1; i < n; i++){
-            leftheight[i] = height[i]>leftheight[i-1]?height[i]:leftheight[i-1];
-        }
-        rightheight[n-1] = height[n-1];
-        for (int i = n-2; i >= 0; i--){
-            rightheight[i] = height[i]>rightheight[i+1]?height[i]:rightheight[i+1];
-        }
-        
+        int[] lmax = new int[n];
+        int[] rmax = new int[n];
+        lmax[0] = height[0];
+        rmax[n-1] = height[n-1];
+        for (int i = 1; i < n; i++)
+            lmax[i] = Math.max(height[i], lmax[i-1]);
+        for (int i = n-2; i >= 0; i--)
+            rmax[i] = Math.max(height[i], rmax[i+1]);
         int sum = 0;
-        for (int i = 1; i < n; i++){
-            int left = leftheight[i];
-            int right = rightheight[i];
-            sum += (left < right ? left:right) - height[i];
-        }
+        for (int i = 1; i < n-1; i++)
+            sum += Math.min(lmax[i],rmax[i]) - height[i];
+        
         return sum;
     }
 }
